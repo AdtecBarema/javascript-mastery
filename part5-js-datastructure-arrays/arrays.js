4fbea7e482dea66c1c1566d42f72f1f8f0f9c4b0
addisu mekonnen marilign <addisumekonnenmarilign@Addisus-MBP.home>
2022-11-17 10:42:17 -0600

    JS Data structure - array

--- part5-js-datastructure-arrays/arrays.js
+++ part5-js-datastructure-arrays/arrays.js
@@ -10,3 +10,15 @@ numbers[0]=44
 console.log(numbers)
 
 numbers.push(60)
+//Arrays Destructuring 
+const name =[
+    "James","Mariam", "Jamal","Aisha","Alex"
+];
+
+
+// const james = name[0];
+// const mairam = name[1];
+const [james, mariam, jemal, ...remaining] = name;
+console.log(james);
+console.log(mariam);
+console.log(remaining);
